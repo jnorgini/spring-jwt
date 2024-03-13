46aa9e52bc006ce9b91ac176feb182a1e307d46b
jnorgini <jnorgini@gmail.com>
2024-03-13 16:43:45 -0300

    feat: SecurityFilter

---
+++ src/main/java/com/norgini/config/SecurityFilter.java
@@ -0,0 +1,54 @@
+package com.norgini.config;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.norgini.repositories.RefreshTokenRepository;
+import com.norgini.repositories.UserRepository;
+import com.norgini.services.TokenService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+	private TokenService service;
+	private UserRepository repository;
+	private RefreshTokenRepository tokenRepository;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		var tokenJWT = tokenRecovery(request);
+
+		if (tokenJWT != null) {
+			var subject = service.getUsername(tokenJWT);
+			tokenRepository.findByToken(tokenJWT).orElseThrow();
+			var usuario = repository.findByUsername(subject);
+
+			var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
+		filterChain.doFilter(request, response);
+
+	}
+
+	private String tokenRecovery(HttpServletRequest request) {
+		var authorizationHeader = request.getHeader("Authorization");
+		if (authorizationHeader != null) {
+			return authorizationHeader.replace("Bearer ", "");
+		}
+		return null;
+	}
+
+}
