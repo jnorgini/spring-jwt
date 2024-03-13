d0b0a29c7711d314dd71e0d61168ca8a5771206b
jnorgini <jnorgini@gmail.com>
2024-03-13 16:44:16 -0300

    feat: SecurityConfiguration

---
+++ src/main/java/com/norgini/config/SecurityConfiguration.java
@@ -0,0 +1,69 @@
+package com.norgini.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandlerImpl;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.norgini.services.UserService;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfiguration {
+
+    private SecurityFilter securityFilter;
+    private UserService userService;
+    private LoginEntryPoint loginEntryPoint;
+
+    @Bean
+    AuthenticationManager getAuthenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        var builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(userService).passwordEncoder(encoder());
+        
+        var authenticationManager = builder.build();
+
+        http.csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .authorizeHttpRequests(requests -> 
+                    requests
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/refresh").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/users/{id}").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/users/{id}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/users").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/users/{id}").permitAll()
+                        .anyRequest().authenticated())
+                .authenticationManager(authenticationManager)
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(handling -> handling
+                    .authenticationEntryPoint(loginEntryPoint)
+                    .accessDeniedHandler(new AccessDeniedHandlerImpl())
+                );
+
+        return http.build();
+    }
+}
