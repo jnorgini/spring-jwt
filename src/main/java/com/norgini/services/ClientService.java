ebfa76ac934bbbd20a6220f4081177bbf67620f2
jnorgini <jnorgini@gmail.com>
2024-03-22 20:29:47 -0300

    refactor: client service

--- src/main/java/com/norgini/services/ClientService.java
+++ src/main/java/com/norgini/services/ClientService.java
@@ -2,13 +2,11 @@ package com.norgini.services;
 
 import java.util.List;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.norgini.dtos.ClientDTO;
 import com.norgini.entities.Client;
+import com.norgini.exceptions.ClientNotFoundException;
 import com.norgini.repositories.ClientRepository;
 
 import lombok.AllArgsConstructor;
@@ -16,34 +14,26 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class ClientService {
-	
+
 	private ClientRepository repository;
-	private ModelMapper mapper;
-	
+
 	@Transactional
-	public Client create(ClientDTO obj) {
-		return repository.save(mapper.map(obj, Client.class));
+	public Client create(Client client) {
+		return repository.save(client);
 	}
-	
+
 	@Transactional
-	public Client update(ClientDTO obj) {
-		Long id = obj.getId();
-		Client product = repository.findById(id).get();
-		product = mapper.map(obj, Client.class);
-		return repository.save(product);
-	}
-	
-	@Transactional
-	public void delete(@NonNull Long id) {
+	public void delete(Long id) {
 		repository.deleteById(id);
 	}
-	
+
+	public Client find(Long id) {
+		return repository.findById(id)
+				.orElseThrow(() -> new ClientNotFoundException(id));
+	}
+
 	public List<Client> findClients() {
 		return repository.findAll();
-	}
-	
-	public Client findClient(@NonNull Long id) {
-		return repository.findById(id).get();
 	}
 
 }
