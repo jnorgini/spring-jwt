cda2f58d2f97a16a54585ba0b7bb498ab1f820ea
jnorgini <jnorgini@gmail.com>
2024-03-22 20:30:25 -0300

    refactor: client controller

--- src/main/java/com/norgini/controllers/ClientController.java
+++ src/main/java/com/norgini/controllers/ClientController.java
@@ -2,10 +2,8 @@ package com.norgini.controllers;
 
 import java.util.List;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.norgini.dtos.ClientDTO;
+import com.norgini.assembler.ClientRequestDisassembler;
+import com.norgini.assembler.ClientResponseAssembler;
+import com.norgini.dtos.ClientRequest;
+import com.norgini.dtos.ClientResponse;
 import com.norgini.entities.Client;
 import com.norgini.services.ClientService;
 
@@ -28,44 +29,39 @@ import lombok.AllArgsConstructor;
 public class ClientController {
 
 	private ClientService service;
-	private ModelMapper mapper;
-
-	@PostMapping
-	public ResponseEntity<ClientDTO> create(@RequestBody @Valid ClientDTO obj) {
-		Client client = service.create(obj);
-		ClientDTO entityToDto = mapper.map(client, ClientDTO.class);
-		return ResponseEntity.status(HttpStatus.CREATED).body(entityToDto);
-	}
-
-	@PutMapping("/{id}")
-	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @Valid @RequestBody ClientDTO obj) {
-		obj.setId(id);
-		Client updatedClient = service.update(obj);
-
-		if (updatedClient != null) {
-			ClientDTO updatedClientDTO = mapper.map(updatedClient, ClientDTO.class);
-			return ResponseEntity.ok().body(updatedClientDTO);
-		} else {
-			return ResponseEntity.notFound().build();
-		}
-	}
-
-	@DeleteMapping("/{id}")
-	public ResponseEntity<?> delete(@PathVariable @NonNull Long id) {
-		service.delete(id);
-		return ResponseEntity.noContent().build();
-	}
+	private ClientResponseAssembler clientResponseAssembler;
+	private ClientRequestDisassembler clientRequestDisassembler;
 
 	@GetMapping
-	public ResponseEntity<List<Client>> getClients() {
+	public ResponseEntity<List<ClientResponse>> getClients() {
 		List<Client> clients = service.findClients();
-		return ResponseEntity.ok(clients);
+		return ResponseEntity.ok(clientResponseAssembler.toCollectModel(clients));
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Client> getClient(@PathVariable @NonNull Long id) {
-		Client client = service.findClient(id);
-		return ResponseEntity.ok(client);
+	public ResponseEntity<ClientResponse> getClient(@PathVariable Long id) {
+		Client currentClient = service.find(id);
+		return ResponseEntity.ok(clientResponseAssembler.toModel(currentClient));
+	}
+
+	@PostMapping
+	public ResponseEntity<ClientResponse> create(@Valid @RequestBody ClientRequest clientRequest) {
+		Client newClient = service.create(clientRequestDisassembler.toDomainObject(clientRequest));
+		return ResponseEntity.status(HttpStatus.CREATED).body(clientResponseAssembler.toModel(newClient));
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<ClientResponse> update(@PathVariable Long id, @Valid @RequestBody ClientRequest clientRequest) {
+		Client currentClient = service.find(id);
+		clientRequestDisassembler.copyToDomainObject(clientRequest, currentClient);
+		Client updatedClient = service.create(currentClient);
+		return ResponseEntity.ok(clientResponseAssembler.toModel(updatedClient));
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
