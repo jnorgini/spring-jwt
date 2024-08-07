f905e583ece624e0af25410d23e256c6d9e27fb7
jnorgini <jnorgini@gmail.com>
2024-08-07 19:48:18 -0300

    fix: UserResponse and delete

--- src/main/java/com/norgini/services/UserService.java
+++ src/main/java/com/norgini/services/UserService.java
@@ -2,8 +2,6 @@ package com.norgini.services;
 
 import java.util.List;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,7 +15,6 @@ import com.norgini.dtos.UserRequest;
 import com.norgini.dtos.UserResponse;
 import com.norgini.entities.User;
 import com.norgini.entities.UserStatus;
-import com.norgini.exceptions.UnauthorizedOperationException;
 import com.norgini.exceptions.UserNotFoundException;
 import com.norgini.repositories.RefreshTokenRepository;
 import com.norgini.repositories.UserRepository;
@@ -45,11 +42,6 @@ public class UserService implements UserDetailsService {
 
 	@Transactional
 	public UserResponse update(Long id, UserRequest userRequest) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		var currentUser = (User) auth.getPrincipal();
-		if (!currentUser.getId().equals(id)) {
-			throw new UnauthorizedOperationException("You do not have permission to update this user.");
-		}
 		User existingUser = repository.findById(id).get();
 		userRequestDisassembler.copyToDomainObject(userRequest, existingUser);
 		String password = new BCryptPasswordEncoder().encode(userRequest.getPassword());
@@ -60,11 +52,6 @@ public class UserService implements UserDetailsService {
 
 	@Transactional
 	public void delete(Long id) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		var currentUser = (User) auth.getPrincipal();
-		if (!currentUser.getId().equals(id)) {
-			throw new UnauthorizedOperationException("You do not have permission to delete this user.");
-		}
 		User user = repository.findById(id).get();
 		refreshTokenRepository.deleteByUser(user);
 		repository.deleteById(id);
@@ -88,7 +75,8 @@ public class UserService implements UserDetailsService {
 			return org.springframework.security.core.userdetails
 					.User.builder()
 					.password(user.getPassword())
-					.username(user.getUsername()).build();
+					.username(user.getUsername())
+					.build();
 		} else {
 			throw new UsernameNotFoundException("");
 		}
